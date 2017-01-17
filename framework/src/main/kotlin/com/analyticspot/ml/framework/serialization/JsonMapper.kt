4c84934f3dda934bf1af9d815d310326309aa498
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-01-16 16:40:35 -0800

    Adds LGPL license.

--- framework/src/main/kotlin/com/analyticspot/ml/framework/serialization/JsonMapper.kt
+++ framework/src/main/kotlin/com/analyticspot/ml/framework/serialization/JsonMapper.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2017 Analytic Spot.
+ * 
+ * This file is part of the ANX ML library.
+ * 
+ * The ANX ML library is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with the ANX ML libarary.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package com.analyticspot.ml.framework.serialization
 
 import com.fasterxml.jackson.core.JsonGenerator
