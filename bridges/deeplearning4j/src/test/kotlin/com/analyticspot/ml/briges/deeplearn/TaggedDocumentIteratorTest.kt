10224096b7d503f7de4acc8cd46260545674ddb3
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-01-26 13:06:27 -0800

    Final test

--- bridges/deeplearning4j/src/test/kotlin/com/analyticspot/ml/briges/deeplearn/TaggedDocumentIteratorTest.kt
+++ bridges/deeplearning4j/src/test/kotlin/com/analyticspot/ml/briges/deeplearn/TaggedDocumentIteratorTest.kt
@@ -78,4 +78,45 @@ class TaggedDocumentIteratorTest {
 
         iter.finish()
     }
+
+    @Test
+    fun testWithManyLabels() {
+        val docs = listOf(
+                listOf("foo", "bar", "baz"),
+                listOf("a", "b", "c", "d", "e", "f"),
+                listOf("AnalyticSpot", "makes", "awesome", "software")
+        )
+        val docCol = ColumnId.create<List<String>>("doc")
+
+        val labels1 = listOf("X", "Y", "Z")
+        val label1Col = ColumnId.create<String>("label1")
+
+        val labels2 = listOf(1, 10, 20)
+        val label2Col = ColumnId.create<Int>("label2")
+
+        val labels3 = listOf("bi", "bim", "bap")
+        val label3Col = ColumnId.create<String>("label3")
+
+        val ds = DataSet.build {
+            addColumn(docCol, docs)
+            addColumn(label1Col, labels1)
+            addColumn(label2Col, labels2)
+            addColumn(label3Col, labels3)
+        }
+
+        val iter = TaggedDocumentIterator(ds, docCol, listOf(label1Col, label2Col, label3Col))
+
+        assertThat(iter.hasNext()).isTrue()
+        assertThat(iter.nextSentence()).isEqualTo(docs[0].joinToString(" "))
+        assertThat(iter.currentLabels()).containsExactly(labels1[0], labels2[0].toString(), labels3[0])
+        assertThat(iter.hasNext()).isTrue()
+        assertThat(iter.nextSentence()).isEqualTo(docs[1].joinToString(" "))
+        assertThat(iter.currentLabels()).containsExactly(labels1[1], labels2[1].toString(), labels3[1])
+        assertThat(iter.hasNext()).isTrue()
+        assertThat(iter.nextSentence()).isEqualTo(docs[2].joinToString(" "))
+        assertThat(iter.currentLabels()).containsExactly(labels1[2], labels2[2].toString(), labels3[2])
+        assertThat(iter.hasNext()).isFalse()
+
+        iter.finish()
+    }
 }
