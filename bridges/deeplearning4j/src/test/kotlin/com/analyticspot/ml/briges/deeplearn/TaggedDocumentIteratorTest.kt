edc773f7eb5eb02b62bcab10680e048d28163dc6
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-01-26 13:03:39 -0800

    More tests.

--- bridges/deeplearning4j/src/test/kotlin/com/analyticspot/ml/briges/deeplearn/TaggedDocumentIteratorTest.kt
+++ bridges/deeplearning4j/src/test/kotlin/com/analyticspot/ml/briges/deeplearn/TaggedDocumentIteratorTest.kt
@@ -22,10 +22,60 @@ class TaggedDocumentIteratorTest {
 
         assertThat(iter.hasNext()).isTrue()
         assertThat(iter.nextSentence()).isEqualTo(docs[0].joinToString(" "))
+        assertThat(iter.currentLabels()).isEmpty()
         assertThat(iter.hasNext()).isTrue()
         assertThat(iter.nextSentence()).isEqualTo(docs[1].joinToString(" "))
+        assertThat(iter.currentLabels()).isEmpty()
         assertThat(iter.hasNext()).isTrue()
         assertThat(iter.nextSentence()).isEqualTo(docs[2].joinToString(" "))
+        assertThat(iter.currentLabels()).isEmpty()
         assertThat(iter.hasNext()).isFalse()
+
+        iter.finish()
+    }
+
+    @Test
+    fun testEmptyIterator() {
+        val docCol = ColumnId.create<List<String>>("doc")
+        val ds = DataSet.build {
+            addColumn(docCol, listOf())
+        }
+
+        val iter = TaggedDocumentIterator(ds, docCol, listOf())
+
+        assertThat(iter.hasNext()).isFalse()
+        iter.finish()
+    }
+
+    @Test
+    fun testWithOneLabel() {
+        val docs = listOf(
+                listOf("foo", "bar", "baz"),
+                listOf("a", "b", "c", "d", "e", "f"),
+                listOf("AnalyticSpot", "makes", "awesome", "software")
+        )
+        val docCol = ColumnId.create<List<String>>("doc")
+
+        val labels = listOf("X", "Y", "Z")
+        val labelCol = ColumnId.create<String>("label")
+        val ds = DataSet.build {
+            addColumn(docCol, docs)
+            addColumn(labelCol, labels)
+        }
+
+        val iter = TaggedDocumentIterator(ds, docCol, listOf(labelCol))
+
+        assertThat(iter.hasNext()).isTrue()
+        assertThat(iter.nextSentence()).isEqualTo(docs[0].joinToString(" "))
+        assertThat(iter.currentLabels()).containsExactly(labels[0])
+        assertThat(iter.hasNext()).isTrue()
+        assertThat(iter.nextSentence()).isEqualTo(docs[1].joinToString(" "))
+        assertThat(iter.currentLabels()).containsExactly(labels[1])
+        assertThat(iter.hasNext()).isTrue()
+        assertThat(iter.nextSentence()).isEqualTo(docs[2].joinToString(" "))
+        assertThat(iter.currentLabels()).containsExactly(labels[2])
+        assertThat(iter.hasNext()).isFalse()
+
+        iter.finish()
     }
 }
