2779e3cf85ae916178b0c471aff9bb4bbf41fa97
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-02-09 16:39:37 -0800

    Checkpoint commit. Want to add feature stuff first.

---
+++ bridges/smile/src/main/kotlin/com/analyticspot/ml/bridges/smile/DataSetConverter.kt
@@ -0,0 +1,11 @@
+package com.analyticspot.ml.bridges.smile
+
+import com.analyticspot.ml.framework.dataset.DataSet
+import smile.data.AttributeDataset
+
+/**
+ * Routines for converting [DataSet] instances to smile's `AttributeDataSet` and back again.
+ */
+fun toSmile(inData: DataSet): AttributeDataset {
+
+}
