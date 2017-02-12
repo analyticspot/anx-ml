5c337082d20e6275de4885d60277da614ea69cf6
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-02-12 11:35:14 -0800

    Almost done but going to make some API changes first.

--- bridges/smile/src/test/kotlin/com/analyticspot/ml/bridges/smile/SmileClassifierTest.kt
+++ bridges/smile/src/test/kotlin/com/analyticspot/ml/bridges/smile/SmileClassifierTest.kt
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory
 import org.testng.annotations.Test
 import smile.classification.ClassifierTrainer
 import smile.classification.DecisionTree
+import smile.data.Attribute
 
 class SmileClassifierTest {
     companion object {
@@ -14,10 +15,10 @@ class SmileClassifierTest {
 
     @Test
     fun testSimpleClassifier() {
-        val dtTrainer = DecisionTree.Trainer()
+        val dtTrainerFactory = { attrs: Array<Attribute> -> DecisionTree.Trainer(attrs) }
         log.info("Type of dtTrainer is: {}", DecisionTree.Trainer::class)
         val targetId = CategoricalFeatureId("foo", false, setOf("a", "b", "c"))
 
-        val trans = SmileClassifier(targetId, dtTrainer)
+        val trans = SmileClassifier(targetId, dtTrainerFactory)
     }
 }
