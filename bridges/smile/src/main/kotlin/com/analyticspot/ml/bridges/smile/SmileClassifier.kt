0ee40270f26d94f3d681ad185611eab65bbf2a33
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-02-12 17:01:47 -0800

    Serialization should work but need to add tests. going to add some convenience methods first.

--- bridges/smile/src/main/kotlin/com/analyticspot/ml/bridges/smile/SmileClassifier.kt
+++ bridges/smile/src/main/kotlin/com/analyticspot/ml/bridges/smile/SmileClassifier.kt
@@ -5,10 +5,14 @@ import com.analyticspot.ml.framework.dataset.DataSet
 import com.analyticspot.ml.framework.datatransform.TargetSupervisedLearningTransform
 import com.analyticspot.ml.framework.description.ColumnId
 import com.analyticspot.ml.framework.metadata.CategoricalFeatureMetaData
+import com.analyticspot.ml.framework.serialization.MultiFileMixedTransform
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.thoughtworks.xstream.XStream
 import org.slf4j.LoggerFactory
 import smile.classification.Classifier
 import smile.classification.ClassifierTrainer
 import smile.data.Attribute
+import java.io.OutputStream
 import java.util.ArrayList
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ExecutorService
@@ -17,14 +21,20 @@ import java.util.concurrent.ExecutorService
  *
  * The returned data set contains the predictions. See [SmileSoftClassifier] if you would also like the posterior
  * probabilities in the output data set.
+ *
+ * Smile recommends the use of Xstream for serialization
+ * (https://github.com/haifengl/smile#user-content-model-serialization). That serializes models as XML. The models can
+ * not be serialized to JSON so we use Xstream. We use [MultiFileMixedFormat] for the serialization even though the
+ * model isn't binary. However, embedding XML in JSON get horribly ugly so this seems to make more sense.
  */
 open class SmileClassifier(targetId: ColumnId<String>,
         private val trainerFactory: (Array<Attribute>) -> ClassifierTrainer<DoubleArray>,
         val predictionColName: String = "predicted")
-    : TargetSupervisedLearningTransform<String>(targetId) {
+    : TargetSupervisedLearningTransform<String>(targetId), MultiFileMixedTransform {
     /**
      * This is the trained model. It won't be set until the training phase of trainTransform is complete.
      */
+    @JsonIgnore
     lateinit var trainedModel: Classifier<DoubleArray>
 
     /**
@@ -70,5 +80,10 @@ open class SmileClassifier(targetId: ColumnId<String>,
         return DataSet.build {
             addColumn(resultCol, predictions, resultMeta)
         }
+    }
+
+    override fun serializeBinaryData(output: OutputStream) {
+        val xstream = XStream()
+        xstream.toXML(trainedModel, output)
     }
 }
