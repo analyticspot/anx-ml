c89067933dd68896b90da8f425d89f5558c1c73c
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-02-12 19:46:40 -0800

    Done.

--- bridges/smile/src/test/kotlin/com/analyticspot/ml/bridges/smile/SmileSoftClassifierTest.kt
+++ bridges/smile/src/test/kotlin/com/analyticspot/ml/bridges/smile/SmileSoftClassifierTest.kt
@@ -4,11 +4,15 @@ import com.analyticspot.ml.framework.datagraph.DataGraph
 import com.analyticspot.ml.framework.dataset.DataSet
 import com.analyticspot.ml.framework.description.ColumnId
 import com.analyticspot.ml.framework.metadata.CategoricalFeatureMetaData
+import com.analyticspot.ml.framework.serialization.GraphSerDeser
+import com.analyticspot.ml.framework.serialization.MultiFileMixedFormat
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.within
 import org.testng.annotations.Test
 import smile.classification.DecisionTree
 import smile.data.Attribute
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.util.concurrent.Executors
 
 class SmileSoftClassifierTest {
@@ -16,7 +20,6 @@ class SmileSoftClassifierTest {
     fun testWorks() {
         val maxNodes = 20
         val dtTrainerFactory = { attrs: Array<Attribute> -> DecisionTree.Trainer(attrs, maxNodes) }
-        // 400 rows of data.
         val trainData = generateDtData()
 
         val treeTransform = SmileSoftClassifier(trainData.targetId, dtTrainerFactory)
@@ -46,7 +49,7 @@ class SmileSoftClassifierTest {
 
         // And just to make sure we're not just using the target value to predict lets build a DataSet that violates the
         // rule and make sure we generate the wrong prediction.
-        val indColId = trainData.data.columnIdWithName<Boolean>("ind")
+        val indColId = ColumnId.create<Boolean>("ind")
         val testDs = DataSet.build {
             addColumn(indColId, listOf(true, false))
         }
@@ -66,6 +69,66 @@ class SmileSoftClassifierTest {
         assertThat(testResult.value(1, treeTransform.classProbsId.generateId("A"))).isCloseTo(0.0, within(0.05))
         assertThat(testResult.value(1, treeTransform.classProbsId.generateId("B"))).isCloseTo(0.2, within(0.05))
         assertThat(testResult.value(1, treeTransform.classProbsId.generateId("C"))).isCloseTo(0.8, within(0.05))
+    }
+
+    @Test
+    fun testCanSerDeser() {
+        val maxNodes = 20
+        val dtTrainerFactory = { attrs: Array<Attribute> -> DecisionTree.Trainer(attrs, maxNodes) }
+        val trainData = generateDtData()
+
+        val treeTransform = SmileSoftClassifier(trainData.targetId, dtTrainerFactory)
+
+        val dg = DataGraph.build {
+            val src = setSource {
+                columnIds += trainData.data.columnIds.filter { it.name != trainData.targetId.name }
+                trainOnlyColumnIds += trainData.targetId
+            }
+
+            val inputSet = removeColumns(src, trainData.targetId)
+
+            val targetSet = keepColumns(src, trainData.targetId)
+
+            val tree = addTransform(inputSet, targetSet, treeTransform)
+
+            result = tree
+        }
+
+        val exec = Executors.newFixedThreadPool(2)
+
+        dg.trainTransform(trainData.data, exec).get()
+
+        // serialize the graph
+        val output = ByteArrayOutputStream()
+        val serDeser = GraphSerDeser()
+        serDeser.registerFormat(MultiFileMixedFormat())
+        serDeser.serialize(dg, output)
+
+        // and deserialize it
+        val deserDg = serDeser.deserialize(ByteArrayInputStream(output.toByteArray()))
+
+        // Make sure it can still make correct predictions
+        val indColId = ColumnId.create<Boolean>("ind")
+        val testDs = DataSet.build {
+            addColumn(indColId, listOf(true, false))
+        }
+
+        val testResult = deserDg.transform(testDs, exec).get()
+        assertThat(testResult.numRows).isEqualTo(2)
+        assertThat(testResult.numColumns).isEqualTo(4)
+
+        // For the first row ind was true so we expect the prediction to be A and the posteriors to be .9, .1, and 0
+        assertThat(testResult.value(0, treeTransform.predictionCol)).isEqualTo("A")
+        assertThat(testResult.value(0, treeTransform.classProbsId.generateId("A"))).isCloseTo(0.9, within(0.05))
+        assertThat(testResult.value(0, treeTransform.classProbsId.generateId("B"))).isCloseTo(0.1, within(0.05))
+        assertThat(testResult.value(0, treeTransform.classProbsId.generateId("C"))).isCloseTo(0.0, within(0.05))
+
+        // For the 2nd row ind was false so we expect the preciction to be C
+        assertThat(testResult.value(1, treeTransform.predictionCol)).isEqualTo("C")
+        assertThat(testResult.value(1, treeTransform.classProbsId.generateId("A"))).isCloseTo(0.0, within(0.05))
+        assertThat(testResult.value(1, treeTransform.classProbsId.generateId("B"))).isCloseTo(0.2, within(0.05))
+        assertThat(testResult.value(1, treeTransform.classProbsId.generateId("C"))).isCloseTo(0.8, within(0.05))
+
     }
 
     // Return type for generateDtData below
