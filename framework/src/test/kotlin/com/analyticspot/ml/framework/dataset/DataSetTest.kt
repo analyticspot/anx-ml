4771ba1856cb468d364d5410ab8865fc9b86e4e4
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-02-14 14:36:30 -0800

    Another good test.

--- framework/src/test/kotlin/com/analyticspot/ml/framework/dataset/DataSetTest.kt
+++ framework/src/test/kotlin/com/analyticspot/ml/framework/dataset/DataSetTest.kt
@@ -24,7 +24,7 @@ class DataSetTest {
         ds.toDelimited(output, true, nullVal, delimiter)
         output.close()
 
-        val outputStr = String(output.toByteArray(), Charset.forName("UTF8"))
+        val outputStr = String(output.toByteArray())
 
         val expected = """
         |c1,c2,c3
@@ -34,5 +34,30 @@ class DataSetTest {
         |""".trimMargin()
 
         assertThat(outputStr).isEqualTo(expected)
+    }
+
+    @Test
+    fun testToDelimitedDoesNotCloseStream() {
+        val col1 = ColumnId.create<Int>("c1")
+        val col2 = ColumnId.create<String>("c2")
+        val col3 = ColumnId.create<Double>("c3")
+        val ds = DataSet.build {
+            addColumn(col1, listOf(1, 2, null))
+            addColumn(col2, listOf("foo", null, "bar"))
+            addColumn(col3, listOf(null, 1.002, 0.0004))
+        }
+
+        val output = ByteArrayOutputStream()
+        ds.toDelimited(output)
+
+        // Now write some more stuff to the stream to ensure it wasn't closed
+        val finalLine = "AND MORE STUFF"
+        val writer = output.writer()
+        writer.write("\n$finalLine")
+        writer.close()
+
+
+        val outputStr = String(output.toByteArray())
+        assertThat(outputStr).endsWith(finalLine)
     }
 }
