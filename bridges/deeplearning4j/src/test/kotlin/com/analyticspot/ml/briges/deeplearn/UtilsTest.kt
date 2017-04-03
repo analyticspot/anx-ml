cf5651eb8b98fe821bb9f43a3e73c31115c948c3
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-04-03 16:29:25 -0700

    Yet more testing goodness.

--- bridges/deeplearning4j/src/test/kotlin/com/analyticspot/ml/briges/deeplearn/UtilsTest.kt
+++ bridges/deeplearning4j/src/test/kotlin/com/analyticspot/ml/briges/deeplearn/UtilsTest.kt
@@ -3,9 +3,15 @@ package com.analyticspot.ml.briges.deeplearn
 import com.analyticspot.ml.framework.dataset.DataSet
 import com.analyticspot.ml.framework.description.ColumnId
 import org.assertj.core.api.Assertions.assertThat
+import org.nd4j.linalg.api.ndarray.INDArray
+import org.slf4j.LoggerFactory
 import org.testng.annotations.Test
 
 class UtilsTest {
+    companion object {
+        private val log = LoggerFactory.getLogger(UtilsTest::class.java)
+    }
+
     @Test
     fun testToMultiDataSetFromSingleDs() {
         val c1 = ColumnId.create<Int>("c1")
@@ -22,10 +28,10 @@ class UtilsTest {
 
         // We should be able to re-use columns in different feature sets (e.g. to pass the same values to different
         // Neural net inputs) and even re-use them as targets (e.g. to train an embedding).
-        val fs1 = listOf(c1, c2)
-        val fs2 = listOf(c2)
-        val fs3 = listOf(c2, c4)
-        val fs4 = listOf(c2, c3, c4)
+        val fs1: List<ColumnId<out Number>> = listOf(c1, c2)
+        val fs2: List<ColumnId<out Number>> = listOf(c2)
+        val fs3: List<ColumnId<out Number>> = listOf(c2, c4)
+        val fs4: List<ColumnId<out Number>> = listOf(c2, c3, c4)
 
         val targs = listOf(c1, c4)
 
@@ -37,6 +43,35 @@ class UtilsTest {
         assertThat(multiDs.features[2].shape()).isEqualTo(arrayOf(3, 2))
         assertThat(multiDs.features[3].shape()).isEqualTo(arrayOf(3, 3))
 
+        assertDataSetsEqual(multiDs.features[0], ds, fs1)
+        assertDataSetsEqual(multiDs.features[1], ds, fs2)
+        assertDataSetsEqual(multiDs.features[2], ds, fs3)
+        assertDataSetsEqual(multiDs.features[3], ds, fs4)
 
+        assertOneHotEqual(multiDs.labels[0], ds.column(c1))
+        assertOneHotEqual(multiDs.labels[1], ds.column(c4))
+    }
+
+    private fun assertDataSetsEqual(nd4jData: INDArray, ds: DataSet, cols: List<ColumnId<out Number>>) {
+        cols.forEachIndexed { colIdx, columnId ->
+            ds.column(columnId).forEachIndexed { rowIdx, value ->
+                assertThat(nd4jData.getDouble(rowIdx, colIdx)).isEqualTo(value?.toDouble())
+            }
+        }
+    }
+
+    private fun assertOneHotEqual(nd4jData: INDArray, targets: Iterable<Int?>) {
+        assertThat(targets).hasSize(nd4jData.rows())
+        targets.forEachIndexed { rowIdx, targetValue ->
+            assertThat(targetValue).isNotNull()
+            log.debug("Checking that {} one-hot encodes {}", nd4jData.getRow(rowIdx), targetValue)
+            for (colIdx in 0.until(nd4jData.columns())) {
+                if (colIdx == targetValue) {
+                    assertThat(nd4jData.getDouble(rowIdx, colIdx)).isEqualTo(1.0)
+                } else {
+                    assertThat(nd4jData.getDouble(rowIdx, colIdx)).isEqualTo(0.0)
+                }
+            }
+        }
     }
 }
