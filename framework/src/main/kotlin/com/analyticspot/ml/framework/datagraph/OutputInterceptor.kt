0f3b42479ba16ccdab73df941172cfbe38505dbe
Oliver Dain <oliver@Olivers-MacBook-Pro.local>
2017-05-05 12:48:33 -0700

    Better comments.

--- framework/src/main/kotlin/com/analyticspot/ml/framework/datagraph/OutputInterceptor.kt
+++ framework/src/main/kotlin/com/analyticspot/ml/framework/datagraph/OutputInterceptor.kt
@@ -10,5 +10,16 @@ import java.util.concurrent.CompletableFuture
  * data computed by the wrapped node.
  */
 interface OutputInterceptor {
+    /**
+     * Called when the execution of the wrapped node is complete.
+     *
+     * @param subIdToData contains the data that was the input to the wrapped node. It is a map from the subscription
+     *     id to the data received by that subscription.
+     * @param execType the type of execution (training or transform) being run
+     * @param output the data computed by the wrapped transform
+     *
+     * @return a new [DataSet]. All graph nodes subscribing to the wrapped node will see the data this returns instead
+     *     of the data returned by the wrapped node.
+     */
     fun intercept(subIdToData: Map<Int, DataSet>, execType: ExecutionType, output: DataSet): CompletableFuture<DataSet>
 }
